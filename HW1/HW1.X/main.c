2b5b2f779e15fb162c5ecc3cb72b8bacd8f83cd1
Mike Wiznitzer <mechanic.wiz@gmail.com>
2018-04-09 21:39:25 -0500

    got the code working at the right frequecy

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -23,7 +23,7 @@
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLIDIV = DIV_12 // divide input clock to be in range 4-5MHz
 #pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
 #pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
 #pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
@@ -54,11 +54,21 @@ int main() {
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
-
+    TRISAbits.TRISA4 = 0;
+    LATAbits.LATA4 = 1;
+    TRISBbits.TRISB4 = 1;
     __builtin_enable_interrupts();
 
     while(1) {
 	// use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 	// remember the core timer runs at half the sysclk
+        while (!PORTBbits.RB4) {
+            
+        }
+        _CP0_SET_COUNT(0);
+        while (_CP0_GET_COUNT() < 2000) {
+            
+        }
+        LATAINV = 0x10;
     }
 }
