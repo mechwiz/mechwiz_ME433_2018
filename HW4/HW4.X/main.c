69dbb0377cac731b9956bf741357880e45f3a4d9
Mike Wiznitzer <mechanic.wiz@gmail.com>
2018-04-22 14:25:35 -0500

    added comments to HW4

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -36,20 +36,7 @@
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
 #pragma config FUSBIDIO = ON // USB pins controlled by USB module
 #pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
-// Demonstrates spi by accessing external ram
-// PIC is the master, ram is the slave
-// Uses microchip 23K256 ram chip (see the data sheet for protocol details)
-// SDO4 -> SI (pin F5 -> pin 5)
-// SDI4 -> SO (pin F4 -> pin 2)
-// SCK4 -> SCK (pin B14 -> pin 6)
-// SS4 -> CS (pin B8 -> pin 1)
-// Additional SRAM connections
-// Vss (Pin 4) -> ground
-// Vcc (Pin 8) -> 3.3 V
-// Hold (pin 7) -> 3.3 V (we don't use the hold function)
-// 
-// Only uses the SRAM's sequential mode
-//
+
 #define CS LATAbits.LATA0       // chip select pin
 
 // send a byte via spi and return the response
@@ -61,29 +48,25 @@ unsigned char spi_io(unsigned char o) {
   return SPI1BUF;
 }
 
-// initialize spi4 and the ram module
+// initialize spi1
 void initSPI1() {
   // set up the chip select pin as an output
-  // the chip select pin is used by the sram to indicate
+  // the chip select pin is used to indicate
   // when a command is beginning (clear CS to low) and when it
   // is ending (set CS high)
   TRISAbits.TRISA0 = 0;
   CS = 1;
-
-  // Master - SPI4, pins are: SDI4(F4), SDO4(F5), SCK4(F13).  
-  // we manually control SS4 as a digital output (F12)
-  // since the pic is just starting, we know that spi is off. We rely on defaults here
  
-  // setup spi4
-  RPA1Rbits.RPA1R = 0b0011;
-  RPA0Rbits.RPA0R = 0b0011;     
+  // setup spi1
+  RPA1Rbits.RPA1R = 0b0011; // set RA1 to be SDO1
+  RPA0Rbits.RPA0R = 0b0011; // set RA0 to be chip select
   SPI1CON = 0;              // turn off the spi module and reset it
   SPI1BUF;                  // clear the rx buffer by reading from it
   SPI1BRG = 0x1;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
   SPI1STATbits.SPIROV = 0;  // clear the overflow bit
   SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
   SPI1CONbits.MSTEN = 1;    // master operation
-  SPI1CONbits.ON = 1;       // turn on spi 4
+  SPI1CONbits.ON = 1;       // turn on spi 1
   SPI1CONbits.MODE16 = 0;
   SPI1CONbits.MODE32 = 0;
 
@@ -96,14 +79,15 @@ void initSPI1() {
 
 void setVoltage(char c,int v) {
   unsigned short t = 0;
-  t = c<<15;
-  t = t | 0b0111000000000000;
-  t = t | v << 2;
+  t = c<<15; // bit shift so that channel is at bit 15
+  t = t | 0b0111000000000000; // get the config bits in there
+  t = t | v << 2;   //bit-shift voltage two places to the left
+                    //stick voltage into message bit
   
   
   CS = 0;                   // enable the ram
-  spi_io(t>>8);             // ram write status
-  spi_io(t&0xff);             // sequential mode (mode = 0b01), hold disabled (hold = 0)
+  spi_io(t>>8);             // write the first 8 bits
+  spi_io(t&0xff);             // write the last 8 bits
   CS = 1;                   // finish the command
   
   
@@ -132,24 +116,26 @@ int main(void) {
     int mid = 0;
     while(1) {
         _CP0_SET_COUNT(0);
-        
+        //start in middle since sin-wave goes negative
         float f = 512.0 + 512.0*sin(i*2.0*3.14/1000.0*10.0);
         setVoltage(0,f);
         i++;
-        
+        //start at 0V and ramp up to 3.3V in 10 cycles, then ramp down for 10 cycles.
         float tri = 10.0*j/1000.0*1023.0;
         setVoltage(1,tri);
-        
+        // If still ramping up
         if (mid == 0 && j< 100) {
+            // If about to reach midpoint
             if (j==99){
-                mid = 1;
+                mid = 1; // set flag that midpoint is reached
             }
-            j++;
-        }else{
+            j++;    //increase voltage
+        }else{  //If after midpoint
+            // If about to reach 0 V
             if (j==1){
-                mid = 0;
+                mid = 0;   //clear midpoint flag
             }
-            j--;
+            j--;    // decrease voltage
         }
         
         // 1ms / (2/48000000) == 24000
