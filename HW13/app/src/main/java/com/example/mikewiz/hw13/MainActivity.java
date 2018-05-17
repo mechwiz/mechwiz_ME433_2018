82008560e9185c3f6822b7fd25f09ab5cb3084c4
Mike Wiznitzer <mechanic.wiz@gmail.com>
2018-05-17 13:11:40 -0500

    added loop to go through rows

--- HW13/app/src/main/java/com/example/mikewiz/hw13/MainActivity.java
+++ HW13/app/src/main/java/com/example/mikewiz/hw13/MainActivity.java
@@ -107,18 +107,21 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
         if (c != null) {
 //            int thresh = 0; // comparison value
             int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
-            int startY = 200; // which row in the bitmap to analyze to read
-            bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
 
-            // in the row, see if there is more green than red
-            for (int i = 0; i < bmp.getWidth(); i++) {
-                if ((green(pixels[i]) - red(pixels[i])) > thresh) {
-                    pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
+            for (int startY = 0; startY< bmp.getHeight();startY++) {
+//                int startY = 200; // which row in the bitmap to analyze to read
+                bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+
+                // in the row, see if there is more green than red
+                for (int i = 0; i < bmp.getWidth(); i++) {
+                    if ((green(pixels[i]) - red(pixels[i])) > thresh) {
+                        pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
+                    }
                 }
-            }
 
-            // update the row
-            bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+                // update the row
+                bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+            }
         }
 
         // draw a circle at some position
