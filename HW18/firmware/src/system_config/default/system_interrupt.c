e0997368a8dd6593e1bdd9fc0b248031672c068f
Mike Wiznitzer <mechanic.wiz@gmail.com>
2018-06-13 04:27:35 -0500

    made PIC code

---
+++ HW18/firmware/src/system_config/default/system_interrupt.c
@@ -0,0 +1,157 @@
+/*******************************************************************************
+ System Interrupts File
+
+  File Name:
+    system_interrupt.c
+
+  Summary:
+    Raw ISR definitions.
+
+  Description:
+    This file contains a definitions of the raw ISRs required to support the
+    interrupt sub-system.
+
+  Summary:
+    This file contains source code for the interrupt vector functions in the
+    system.
+
+  Description:
+    This file contains source code for the interrupt vector functions in the
+    system.  It implements the system and part specific vector "stub" functions
+    from which the individual "Tasks" functions are called for any modules
+    executing interrupt-driven in the MPLAB Harmony system.
+
+  Remarks:
+    This file requires access to the systemObjects global data structure that
+    contains the object handles to all MPLAB Harmony module objects executing
+    interrupt-driven in the system.  These handles are passed into the individual
+    module "Tasks" functions to identify the instance of the module to maintain.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2011-2014 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "system/common/sys_common.h"
+#include "app.h"
+#include "system_definitions.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Interrupt Vector Functions
+// *****************************************************************************
+// *****************************************************************************
+
+ 
+void __ISR(_USB_1_VECTOR, ipl4AUTO) _IntHandlerUSBInstance0(void)
+{
+    DRV_USBFS_Tasks_ISR(sysObj.drvUSBObject);
+}
+
+void __ISR(_TIMER_4_VECTOR, IPL4SOFT) Timer4ISR(void) {
+  // code for PI control goes here
+    static float Eint_left = 0;
+    static float Eint_right = 0;
+    float Kp_left = 1.5, Ki_left = 0.1;
+    float Kp_right = 1.5, Ki_right = 0.1;
+    float left_ref = 0, right_ref = 0;
+    int MAX_DUTY = 40;
+    float kp = 1;
+    
+    int error = rxVal - 319; // 240 means the dot is in the middle of the screen
+    if (error<0) { // slow down the left motor to steer to the left
+        error  = -error;
+        left_ref = MAX_DUTY - kp*error;
+        right_ref = MAX_DUTY;
+        if (left_ref < 0){
+            left_ref = 0;
+        }
+    }
+    else { // slow down the right motor to steer to the right
+        right_ref = MAX_DUTY - kp*error;
+        left_ref = MAX_DUTY;
+        if (right_ref<0) {
+            right_ref = 0;
+        }
+    }
+    
+    int leftcounts = TMR3;
+    TMR3 = 0;
+    int rightcounts = TMR5;
+    TMR5 = 0;
+    
+    float evel_left = left_ref - leftcounts;
+    float evel_right = right_ref - rightcounts;
+    
+    Eint_left+=evel_left;
+    Eint_right+=evel_right;
+    
+    if (Eint_left < -3){
+        Eint_left = -3;
+    }
+    
+     if (Eint_left > 3){
+        Eint_left = 3;
+    }
+    
+    if (Eint_right < -3){
+        Eint_right = -3;
+    }
+    
+    if (Eint_right > 3){
+        Eint_right = 3;
+    }
+    
+    float u_left = Kp_left*evel_left + Ki_left*Eint_left;
+    float u_right = Kp_right*evel_right + Ki_right*Eint_right;
+    
+    if (u_left < 0){
+        u_left = 0;
+    }
+    
+     if (u_left > 100){
+        u_left = 100;
+    }
+    
+    if (u_right < 0){
+        u_right = 0;
+    }
+    
+    if (u_right > 100){
+        u_right = 100;
+    }
+    
+    OC4RS = (int)(u_left/100.0*2400.0+0.5);
+    OC1RS = (int)(u_right/100.0*2400.0+0.5);
+    IFS0bits.T4IF = 0; // clear interrupt flag, last line
+}
+/*******************************************************************************
+ End of File
+*/
